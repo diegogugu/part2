28e05e329e3d6e7ee23287b44cf033f5a0d3cf9a
Diego Longo <diego.longo@mobilesoft.it>
2022-05-05 18:00:03 +0200

    - finished all exercises

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,22 +2,23 @@ import { useState, useEffect } from 'react';
 import Filter from './Filter';
 import PersonForm from './PersonForm';
 import Persons from './Persons';
-import axios from 'axios'
-
+import personService from './services/persons';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
-  const [filteredPersons, setFilteredPersons] = useState(persons)
-  const [filteredValue, setFilteredValue] = useState('')
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+  const [filteredPersons, setFilteredPersons] = useState(persons);
+  const [filteredValue, setFilteredValue] = useState('');
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const getPerson = () => {
-    axios
-    .get('http://localhost:3001/persons')
-    .then(response => {
-      const persons = response.data;
-      setPersons(persons);
-    }, err => {setPersons([{name: 'error loading persons'}])})
+    personService
+      .getAll()
+      .then(response => {
+        const persons = response.data;
+        setPersons(persons);
+      }, err => { setPersons([{ name: 'error loading persons' }]) })
   }
 
   useEffect(() => getPerson, [])
@@ -26,12 +27,19 @@ const App = () => {
     event.preventDefault();
     if (newName && newName !== '' && newNumber && newNumber !== '') {
       if (personDoesntExists(newName)) {
-        setPersons([...persons, { name: newName, number: newNumber, id: Date.now() }]);
-        setNewName('');
-        setNewNumber('');
+        personService.create({ name: newName, number: newNumber, id: Date.now() })
+          .then(response => {
+            setPersons([...persons, response.data]);
+            setNewName('');
+            setNewNumber('');
+            setSuccessMessage(`Added ${response.data.name}`)
+            setTimeout(() => {
+              setSuccessMessage('');
+            }, 2000)
+          }, err => alert(err));
         console.log(persons);
       } else {
-        alert(`${newName.toLowerCase()} is already added to phonebook`);
+        updateUser();
       }
     }
   }
@@ -52,14 +60,41 @@ const App = () => {
     setFilteredPersons(persons.filter(persona => persona.name.toLowerCase().includes(event.target.value.toLowerCase())));
   }
 
+  const handleDelete = (personToDelete) => {
+    if (window.confirm(`Delete ${personToDelete.name}?`)) {
+      personService.remove(personToDelete.id)
+        .then(response => {
+          setPersons(persons.filter(persona => persona.id !== personToDelete.id));
+        }, error => {
+          setErrorMessage(`Information of ${personToDelete.name} has already been removed from server`);
+          setTimeout(() => setErrorMessage(''), 5000);
+        });
+    }
+  }
+
+  const updateUser = () => {
+    if (window.confirm(`${newName.toLowerCase()} is already added to phonebook, replace the old number with a new one?`)) {
+      const found = { name: newName, number: newNumber, id: persons.find(persona => persona.name.toLowerCase() === newName.toLowerCase()).id };
+      personService.update(found).then(res => {
+        setPersons(persons.map(item => item.id === res.data.id ? res.data : item))
+        setSuccessMessage(`Added ${res.data.name}`)
+        setTimeout(() => {
+          setSuccessMessage('');
+        }, 2000)
+      });
+    }
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <>{successMessage ? <div className='success'>{successMessage}</div>: <></>}</>
+      <>{errorMessage ? <div className='error'>{errorMessage}</div>: <></>}</>
       <Filter handleFilterValues={handleFilterValues} filteredValue={filteredValue} />
       <h2>add a new</h2>
       <PersonForm addName={addName} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} newName={newName} newNumber={newNumber} />
       <h2>Numbers</h2>
-      <Persons persons={persons} filteredValue={filteredValue} filteredPersons={filteredPersons} />
+      <Persons persons={persons} filteredValue={filteredValue} filteredPersons={filteredPersons} handleDelete={handleDelete} />
     </div>
   )
 }
--- phonebook/src/Persons.js
+++ phonebook/src/Persons.js
@@ -1,8 +1,8 @@
-const Persons = ({filteredValue, filteredPersons, persons}) => {
+const Persons = ({ filteredValue, filteredPersons, persons, handleDelete }) => {
     return (
-       <> {filteredValue ?
-            filteredPersons.map((persona, index) => (<p key={persona.name + index}>{persona.name} {persona.number}</p>)) :
-            persons.map((persona, index) => (<p key={persona.id}>{persona.name} {persona.number}</p>))}</>
+        <> {filteredValue ?
+            filteredPersons.map((persona, index) => (<p key={persona.name + index}>{persona.name} {persona.number}<button onClick={() => handleDelete(persona)}>Delete</button></p>)) :
+            persons.map((persona, index) => (<p key={persona.id}>{persona.name} {persona.number}<button onClick={() => handleDelete(persona)}>Delete</button></p>))}</>
     )
 }
 
